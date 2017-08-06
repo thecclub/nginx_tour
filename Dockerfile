84b4c6c14681bb7554aaa31c3c2fe94fef6db744
Abhijeet De <abhijeet3192@gmail.com>
2017-08-06 18:20:43 +0530

    Done till add_text task

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM ubuntu:latest
 
 RUN apt-get update
-RUN apt-get install -y  git curl
+RUN apt-get install -y  git curl nano
 RUN mkdir /home/c_club
 ADD scripts /home
 RUN echo "echo \"Hello,  Welcome\"" >> /root/.bashrc
--- scripts/state_checker.sh
+++ scripts/state_checker.sh
@@ -1,17 +1,26 @@
-if [ "$tour_state" = "start" ]; then    
-        if [ "$PWD" = /home/c_club ]; then
+add_text_string="Hello World!"
+
+if [ "$tour_state" == "start" ]; then    
+        if [ "$PWD" == /home/c_club ]; then
           export tour_state="add_file"
         else
           echo "Go to this /home/c_club"
         fi
 fi
-if [ "$tour_state" = "add_file" ]; then
+if [ "$tour_state" == "add_file" ]; then
         if [ -e /home/c_club/abc ]; then
           export tour_state="add_text"
         else
           echo "You need to add a file named abc in the current folder"
         fi
 fi
-if [ "$tour_state" = "add_text" ]; then
-        echo "done"
+if [ "$tour_state" == "add_text" ]; then
+        if [[ "$( cat /home/c_club/abc )" == $add_text_string ]]; then
+          export tour_state="finished"
+        else
+          echo "In file abc write: Hello World!"
+        fi
+fi
+if [ "$tour_state" == "finished" ]; then
+        echo "Nice job! You have completed this exercise"
 fi
