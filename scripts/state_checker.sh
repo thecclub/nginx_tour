b1a7bc72492793ddc5da814b542d6030d6757087
Abhijeet De <abhijeet3192@gmail.com>
2017-08-10 16:51:07 +0530

    Adding vim + small refactor for changing state

--- scripts/state_checker.sh
+++ scripts/state_checker.sh
@@ -4,12 +4,16 @@ function move_to_c_club(){
         cd /home/c_club
 }
 
+function change_state(){
+        export tour_state="$1"
+        export number_of_trials=0
+}
+
 if [ "$tour_state" == "start" ]; then    
         current_state="start"
 
         if [ "$PWD" == /home/c_club ]; then
-          export tour_state="add_file"
-          export number_of_trials=0
+          change_state "add_file"
         else
           echo "Go to this /home/c_club"
           number_of_trials=$((number_of_trials+ 1))
@@ -18,8 +22,8 @@ if [ "$tour_state" == "start" ]; then
         if [ "$tour_state" == "$current_state" ] && [ "$number_of_trials" -gt 3 ]; then
           echo "You had to do: cd /home/c_club"
           move_to_c_club
-          export tour_state="add_file"
-          export number_of_trials=0
+
+          change_state "add_file"
         fi
 fi
 
@@ -27,8 +31,7 @@ if [ "$tour_state" == "add_file" ]; then
         current_state="add_file"
 
         if ! [ -d /home/c_club/abc ] && [ -e /home/c_club/abc ]; then
-          export tour_state="add_text"
-          export number_of_trials=0
+          change_state "add_text"
         else
           echo "You need to add a file named abc in the current folder"
           number_of_trials=$((number_of_trials+ 1))
@@ -37,8 +40,8 @@ if [ "$tour_state" == "add_file" ]; then
         if [ "$tour_state" == "$current_state" ] && [ "$number_of_trials" -gt 3 ]; then
           echo "You had to do: touch abc"
           touch /home/c_club/abc
-          export tour_state="add_text"
-          export number_of_trials=0
+
+          change_state "add_text"
         fi
 fi
 
@@ -46,8 +49,7 @@ if [ "$tour_state" == "add_text" ]; then
         current_state="add_text"
         
         if [[ "$( cat /home/c_club/abc )" == $add_text_string ]]; then
-          export tour_state="install_nginx"
-          export number_of_trials=0
+          change_state "install_nginx"
         else
           echo "In file abc write: Hello World!"
           number_of_trials=$((number_of_trials+ 1))
@@ -56,8 +58,7 @@ if [ "$tour_state" == "add_text" ]; then
         if [ "$tour_state" == "$current_state" ] && [ "$number_of_trials" -gt 3 ]; then
           echo "You had to do: nano abc. Then write Hello World! "
           echo "Hello World!" >>  /home/c_club/abc
-          export tour_state="install_nginx"
-          export number_of_trials=0
+          change_state "install_nginx"
         fi
 fi
 
@@ -65,8 +66,7 @@ if [ "$tour_state" == "install_nginx" ]; then
         current_state="install_nginx"
 
         if [ -x /usr/sbin/nginx ]; then
-          export tour_state="stop_nginx"
-          export number_of_trials=0
+          change_state "stop_nginx"
           service nginx start
         else
           echo "Install Nginx"
@@ -77,8 +77,7 @@ if [ "$tour_state" == "install_nginx" ]; then
           echo "You had to do: apt-get install -y nginx"
           apt-get install -y nginx
           service nginx start
-          export tour_state="stop_nginx"
-          export number_of_trials=0
+          change_state "stop_nginx"
         fi
 fi
 
@@ -88,8 +87,7 @@ if [ "$tour_state" ==  "stop_nginx" ]; then
 
         if (( $(ps -ef | grep -v grep | grep $service | wc -l) == 0 ))
         then
-          export tour_state="start_nginx"
-          export number_of_trials=0
+          change_state "start_nginx"
         else
           echo "Stop the Nginx service"
           number_of_trials=$((number_of_trials+ 1))
@@ -98,8 +96,7 @@ if [ "$tour_state" ==  "stop_nginx" ]; then
         if [ "$tour_state" == "$current_state" ] && [ "$number_of_trials" -gt 3 ]; then
           echo "You had to do: service nginx stop"
           service nginx stop
-          export tour_state="start_nginx"
-          export number_of_trials=0
+          change_state "start_nginx"
         fi
 fi
 
@@ -109,8 +106,7 @@ if [ "$tour_state" ==  "start_nginx" ]; then
 
         if (( $(ps -ef | grep -v grep | grep $service | wc -l) > 0 ))
         then
-          export tour_state="finished"
-          export number_of_trials=0
+          change_state "finished"
         else
           echo "Start the Nginx service"
           number_of_trials=$((number_of_trials+ 1))
@@ -119,8 +115,8 @@ if [ "$tour_state" ==  "start_nginx" ]; then
         if [ "$tour_state" == "$current_state" ] && [ "$number_of_trials" -gt 3 ]; then
           echo "You had to do: service nginx stop"
           service nginx stop
-          export tour_state="finished"
-          export number_of_trials=0
+
+          change_state "finished"
         fi
 fi
 
